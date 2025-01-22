0a6860c6018b0bd80342c3c7ff0f8685c1f4d2dc
giogio8427 <andrea.giostri@polimi.it>
2025-01-22 17:36:03 +0100

    ok...slab (lateral surface to be drawn). Add file to repository

--- analytical/analytical.py
+++ analytical/analytical.py
@@ -174,14 +174,22 @@ while (newSim=='y'):
     theta_profiles[:,0] = thetaRo  # Surface temperature
     theta_profiles[:,1] = thetaR   # Center temperature
     theta_profiles[:,2] = thetaMid # Middle point temperature
-    thetaRTime=np.zeros((nDiscrR, nDiscrR, len(t)))
+    if geom=="sphere":
+        thetaRTime=np.zeros((nDiscrR, nDiscrR, len(t)))
+    elif geom=="slab":
+        thetaRTime=np.zeros((2*nDiscrR-1, nDiscrR, len(t)))
+    
     thetaRadius=np.zeros((len(rr), len(t)))
     dThetaMin=np.zeros((len(rr), len(t)))
     for ii in range(0,len(rr),1):
         thetaRadius[ii,:],dThetaMin[ii,:] = theta(rr[ii]/ro, b, rootsVal, Bi, Fo) 
 
     for ii in range(len(rr)):
-        thetaRTime[:,ii,:]=thetaRadius[:,:]
+        if geom=="sphere":
+            thetaRTime[:,ii,:]=thetaRadius[:,:]
+        elif geom=="slab":
+            thetaRTime[nDiscrR:,ii,:]=thetaRadius[1:,:]
+            thetaRTime[0:nDiscrR,ii,:]=np.flipud(thetaRadius[:,:])
 
     # Lumped Capacitance Solution
     thetaLump, BiLump, FoLump, Lc=thetaLumped(ro, b, h,kw,alpha,t)
@@ -264,8 +272,22 @@ while (newSim=='y'):
     ccMinLump=np.min((thetaLump[:])*thetaIn+Tinf)
     ccMaxLump=np.max((thetaLump[:])*thetaIn+Tinf)
 
+    vertices = np.array([
+        [-ro, -0.5, -0.5],
+        [ro, -0.5, -0.5],
+        [ro, 0.5, -0.5],
+        [-ro, 0.5, -0.5],
+        [-ro, -0.5, 0.5],
+        [ro, -0.5, 0.5],
+        [ro, 0.5, 0.5],
+        [-ro, 0.5, 0.5]
+    ])
+
+
     if geom=="sphere":
         X,Y,Z=pltSphere.sphereCoord(radius=ro, resolution=nDiscrR)
+        YA=Y*0.0
+        YB=Y
         X1,Y1,Z1=pltSphere.circleCoord(rr)
         tt=pltSphere.plotCircle(X1,Y1,Z1, clrMatrix=thetaRTime[:,:,timeSel]*thetaIn+Tinf, 
                                 ccLim=(ccMin, ccMax), colorMap=cMap)
@@ -274,15 +296,16 @@ while (newSim=='y'):
                                 ccLim=(ccMinLump, ccMaxLump),colorMap=cMap)
         tt4=pltSphere.plotSphere(X,Y,Z, clrMatrix=np.ones(X.shape)*thetaLump[timeSel]*thetaIn+Tinf, ccLim=(ccMinLump, ccMaxLump),colorMap=cMap)
     elif geom=='slab':
-      X,Y,Z=pltSphere.prismCoord(ro)
+      X,Y,Z=pltSphere.prismCoord(ro, resolution=nDiscrR)
       X1,Y1,Z1=pltSphere.rectCoord(rr)
-      tt=pltSphere.plotRect(X1,Y1,Z1, clrMatrix=thetaRTime[:,:,timeSel]*thetaIn+Tinf, 
+      YA=Y1*0.0
+      tt,Xtemp,Ytemp,Ztemp=pltSphere.plotRect(X1,Y1,Z1, clrMatrix=thetaRTime[:,:,timeSel]*thetaIn+Tinf, 
                                 ccLim=(ccMin, ccMax), colorMap=cMap)
-      tt2=pltSphere.plotPrism(X,Y,Z, clrMatrix=np.ones(X.shape)*thetaRo[timeSel]*thetaIn+Tinf, ccLim=(ccMin, ccMax),colorMap=cMap)
-      tt3=pltSphere.plotPrism(X,Y*0.0,Z, clrMatrix=np.ones(X.shape)*thetaLump[timeSel]*thetaIn+Tinf, 
+      tt2,Xtemp,Ytemp,Ztemp=pltSphere.drawRectPrismFromVertices(vertices, clrMatrix=thetaRo[timeSel]*thetaIn+Tinf,ccLim=(ccMin, ccMax),colorMap=cMap)
+      tt3,Xtemp,Ytemp,Ztemp=pltSphere.plotRect(X1,Y1,Z1, clrMatrix=np.ones(X.shape)*thetaLump[timeSel]*thetaIn+Tinf, 
                                 ccLim=(ccMinLump, ccMaxLump),colorMap=cMap)
-      tt4=pltSphere.plotPrism(X,Y,Z, clrMatrix=np.ones(X.shape)*thetaLump[timeSel]*thetaIn+Tinf, ccLim=(ccMinLump, ccMaxLump),colorMap=cMap)   
-
+      tt4,Xtemp,Ytemp,Ztemp=pltSphere.drawRectPrismFromVertices(vertices, clrMatrix=thetaLump[timeSel]*thetaIn+Tinf,ccLim=(ccMinLump, ccMaxLump),colorMap=cMap) 
+      YB=Ytemp  
     elif geom=="cylinder":
         qq=0
 
@@ -379,12 +402,12 @@ while (newSim=='y'):
                 # Update surface colors for both spheres
                 "surfacecolor": [
                     thetaRTime[:,:,j]*thetaIn+Tinf,  # For the sphere plot
-                    np.ones(X.shape)*thetaRo[j]*thetaIn+Tinf,
-                    np.ones(X.shape)*thetaLump[j]*thetaIn+Tinf,  # For the sphere plot
-                    np.ones(X.shape)*thetaLump[j]*thetaIn+Tinf  # Keep time evolution plot static  # Keep time evolution plot static
+                    np.ones(YB.shape)*thetaRo[j]*thetaIn+Tinf,
+                    np.ones(YA.shape)*thetaLump[j]*thetaIn+Tinf,  # For the sphere plot
+                    np.ones(YB.shape)*thetaLump[j]*thetaIn+Tinf  # Keep time evolution plot static  # Keep time evolution plot static
                 ],
                 # Update temperature profile scatter plot
-                "y": [Y*0.0, Y, Y*0.0, Y,thetaRadiusTot[:,j]*thetaIn+Tinf,np.repeat(thetaLump[j],2)*thetaIn+Tinf,np.repeat(Tinf,2),  # For the temperature profile plot
+                "y": [YA, YB, YA, YB,thetaRadiusTot[:,j]*thetaIn+Tinf,np.repeat(thetaLump[j],2)*thetaIn+Tinf,np.repeat(Tinf,2),  # For the temperature profile plot
                     thetaRo*thetaIn+Tinf,thetaR*thetaIn+Tinf ,thetaMid*thetaIn+Tinf, thetaLump*thetaIn+Tinf, np.repeat(Tinf,2)]   # Keep time evolution plot static
             }, {
                 "title": f"Temperature Distribution - Time Step: {t[j]} - Energy [J] : {energy[j]:.2f} E/E_Lumped: {energyRatio[j]:.3f}",
